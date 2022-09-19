3a038a08d0be841ddf19d9e89db740fbea861491
Cristina <criss.ms7@gmail.com>
2022-09-19 09:42:01 -0500

    add bbww

---
+++ one_step/submit_mxmy_bbWW.py
@@ -0,0 +1,37 @@
+username = "cmantill"
+eosdir = "/store/group/lpcpfnano/miniaod/2017/XHYPrivate/"
+
+massX = [1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400, 2500, 2600, 2800, 3000, 3500, 4000]
+massY = [60, #70, 80, 90, 
+         100, 125, 150, 190, 250, 300, 350, 400, 450, 
+         500, 600, 700, 800, 900, 1000, # 1100,
+         1200, # 1300, 
+         1400, 1600, 1800, 2000, 2200, 2400,
+         #2500, 2600, 2800
+     ]
+
+#for testing
+massY=[60]
+
+massX_dict = {
+    "cmantill": [1000, 1200, 1400, 1600],
+    "rkansal": [1800, 2000, 2200, 2400],
+    "amitav": [2500, 2600, 2800, 3000],
+}
+
+import os
+
+with open("inputs/NMSSM_XToYH_MX_MY_HTo2bYTo2W_hadronicDecay_cfg.py") as f:
+    template = f.read()
+    
+for mX in massX_dict[username]:
+    for mY in massY:
+        if mY < mX+125:
+            with open("inputs/NMSSM_XToYH_MX%i_MY%i_HTo2bYTo2W_hadronicDecay_cfg.py"%(mX,mY),"w") as f:
+                f.write(template.format(mxx=mX, myy=mY))
+
+            cmd = f'python multi_crab_submit_onestep.py --name NMSSM_XToYH_MX{mX}_MY{mY}_HTo2bYTo2W_hadronicDecay'
+            cmd += f' --config NMSSM_XToYH_MX{mX}_MY{mY}_HTo2bYTo2W_hadronicDecay_cfg.py  --begin-seed 0'
+            cmd += f' --eosdir {eosdir} --site T3_US_FNALLPC' 
+            print(cmd)
+            # os.system(cmd)

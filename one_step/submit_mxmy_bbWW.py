1f3e084151e91b73768cec9659261b24f2fbeab0
Cristina <criss.ms7@gmail.com>
2022-09-26 05:02:48 -0500

    fix typo and add masses

--- one_step/submit_mxmy_bbWW.py
+++ one_step/submit_mxmy_bbWW.py
@@ -1,7 +1,9 @@
 username = "cmantill"
 eosdir = "/store/group/lpcpfnano/miniaod/2017/XHYPrivate/"
 
-massX = [1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400, 2500, 2600, 2800, 3000, 3500, 4000]
+# masses in X
+massX = [650, 700, 1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400, 2600, 2800, 3000, 3500, 4000]
+# masses in Y (some are commented )
 massY = [60, #70, 80, 90, 
          100, 125, 150, 190, 250, 300, 350, 400, 450, 
          500, 600, 700, 800, 900, 1000, # 1100,
@@ -10,13 +12,10 @@ massY = [60, #70, 80, 90,
          #2500, 2600, 2800
      ]
 
-#for testing
-massY=[60]
-
 massX_dict = {
     "cmantill": [1000, 1200, 1400, 1600],
-    "rkansal": [1800, 2000, 2200, 2400],
-    "amitav": [2500, 2600, 2800, 3000],
+    "rkansal": [650, 700, 1800, 2000, 2200],
+    "amitav": [2400, 2600, 2800, 3000],
 }
 
 import os
@@ -31,7 +30,6 @@ for mX in massX_dict[username]:
                 f.write(template.format(mxx=mX, myy=mY))
 
             cmd = f'python multi_crab_submit_onestep.py --name NMSSM_XToYH_MX{mX}_MY{mY}_HTo2bYTo2W_hadronicDecay'
-            cmd += f' --config NMSSM_XToYH_MX{mX}_MY{mY}_HTo2bYTo2W_hadronicDecay_cfg.py  --begin-seed 0'
+            cmd += f' --config NMSSM_XToYH_MX{mX}_MY{mY}_HTo2bYTo2W_hadronicDecay_cfg  --begin-seed 0'
             cmd += f' --eosdir {eosdir} --site T3_US_FNALLPC' 
-            print(cmd)
-            # os.system(cmd)
+            os.system(cmd)

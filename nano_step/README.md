0cda7e20605276ac522c9e8e4fdd7ac8622bf69a
Cristina <criss.ms7@gmail.com>
2023-08-05 12:43:47 -0500

    add recipe for v11

---
+++ nano_step/README.md
@@ -0,0 +1,60 @@
+# Private NanoAOD
+
+## Recipe for NanoAODv11 with modified PNet training for AK4 (and no ParT scores for AK4)
+
+```
+cmsrel CMSSW_12_6_4
+cd CMSSW_12_6_4/src
+eval `scram runtime -sh`
+scram b
+```
+
+# Modify NanoAOD
+```
+git cms-addpkg PhysicsTools/NanoAOD
+git clone -b nanov11-btagak4 https://github.com/cmantill/generateCMS.git
+cp generateCMS/nano_step/LeptonInJetProducer.cc PhysicsTools/NanoAOD/plugins/LeptonInJetProducer.cc
+cp generateCMS/nano_step/jetsAK4_Puppi_cff.py PhysicsTools/NanoAOD/python/jetsAK4_Puppi_cff.py
+```
+
+Note: At some point, we	will switch to ReReco (Partial,	for ABCD)
+https://cms-pdmv.cern.ch/pmp/historical?r=27Jun2023
+Run3_2022_rereco, 124X_dataRun3_v15
+
+2022-ABCDPrompt
+```
+cmsDriver.py --python_filename test_nanoTuples_data2022_ABCDPrompt_cfg.py --eventcontent NANOAOD --datatier NANOAOD --customise_commands "process.options.wantSummary = cms.untracked.bool(True)" \
+--fileout file:nano_data2022_PromptNano.root \
+--conditions 124X_dataRun3_PromptAnalysis_v1 --step NANO --scenario pp \
+--filein /store/data/Run2022D/MuonEG/MINIAOD/PromptReco-v1/000/357/542/00000/750cf639-99bb-401a-a9b2-2b82d17a3082.root \
+--era Run3,run3_nanoAOD_124 --nThreads 4 --no_exec --data -n 100
+```
+
+2022-EFGPrompt
+```
+# adapted from: https://cmsweb.cern.ch/couchdb/reqmgr_config_cache/10fc675e782eab01d6a5188185536e42/configFile
+# adapted from Marina
+cmsDriver.py --python_filename test_nanoTuples_data2022_EFGPrompt_cfg.py --eventcontent NANOAOD --datatier NANOAOD --customise Configuration/DataProcessing/Utils.addMonitoring \
+--fileout file:nano_data2022_PromptNano.root \
+--conditions 124X_dataRun3_Prompt_v10 --step NANO --scenario pp \
+--filein /store/data/Run2022G/MuonEG/MINIAOD/PromptReco-v1/000/362/365/00000/c54ae566-34fd-4c05-87d8-b011b542ecc4.root \
+--era Run3,run3_nanoAOD_124 --nThreads 4 --no_exec --data -n 100
+```
+
+2022-MC Run3Summer22:
+```
+cmsDriver.py --python_filename test_nanoTuples_mc2022.py --eventcontent NANOAODSIM --datatier NANOAODSIM \
+--fileout file:nano_mc2022_v11.root \
+--conditions 126X_mcRun3_2022_realistic_v2 --step NANO --scenario pp \
+--filein /store/mc/Run3Summer22MiniAODv3/GluGluHto2B_PT-200_M-125_TuneCP5_13p6TeV_powheg-minlo-pythia8/MINIAODSIM/124X_mcRun3_2022_realistic_v12-v2/40000/06ecc7c6-d1d0-4ae5-8438-c3dfd666a6e7.root \
+--era Run3,run3_nanoAOD_124 --nThreads 4 --no_exec --mc -n 100
+```
+
+2022-MC Run3Summer22EE:
+```
+cmsDriver.py --python_filename test_nanoTuples_mc2022EE.py --eventcontent NANOAODSIM --datatier NANOAODSIM \
+--fileout file:nano_mc2022EE_v11.root \
+--conditions 126X_mcRun3_2022_realistic_postEE_v1 --step NANO --scenario pp \
+--filein /store/mc/Run3Summer22EEMiniAODv3/GluGluHto2B_PT-200_M-125_TuneCP5_13p6TeV_powheg-minlo-pythia8/MINIAODSIM/124X_mcRun3_2022_realistic_postEE_v1-v2/40000/01eb51b6-9cf1-44a5-8aa4-df1b60c1ccd5.root \
+--era Run3,run3_nanoAOD_124 --nThreads 4 --no_exec --mc	-n 100
+```
---
+++ nano_step/submit.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+#from input_crab_data import dataset_files
+import yaml
+import datetime
+from fnmatch import fnmatch
+from argparse import ArgumentParser
+from multiprocessing import Process
+import copy
+
+from CRABClient.UserUtilities import config, ClientException
+from CRABAPI.RawCommand import crabCommand
+from CRABClient.ClientExceptions import ClientException
+
+sys.path.append(".")
+
+output_site = "T3_US_FNALLPC"
+requestname_base = "privatenano"
+production_tag = "v12"
+
+if __name__ == '__main__':
+
+    def submit(config):
+        print("DEBUG : In submit()")
+        try:
+            crabCommand('submit', config=config)
+        except ClientException as cle:
+            print("Failed submitting task: %s" % (cle))
+
+    parser = ArgumentParser()
+    parser.add_argument('-y', '--yaml', default = 'samples_datatest.yaml', help = 'File with dataset descriptions')
+    parser.add_argument('--username', required=True, help = 'Username')
+    args = parser.parse_args()
+
+    output_lfn_base = "/store/group/lpcdihiggsboost/nanov12/{username}".format(
+        username=args.username
+    )
+    with open(args.yaml) as f:
+        doc = yaml.safe_load(f) # Parse YAML file
+        defaults = doc['defaults'] if 'defaults' in doc else {}
+
+        print(doc)
+        for sample in sorted(doc["samples"].keys()):
+            info = copy.deepcopy(defaults)
+            info.update(doc["samples"][sample])
+            print("\n\n*** Sample {} ***".format(sample))
+
+            for dataset_shortname, dataset in info['datasets'].items():            
+                print("\n*** Submitting {}: {}".format(dataset_shortname, dataset))
+
+                isMC = info.get("isMC", None)
+                if isMC == None:
+                    raise ValueError("Please specify parameter isMC")
+
+                this_config = config()
+
+                this_config.section_('General')
+                this_config.General.transferOutputs = True
+                this_config.General.transferLogs = True
+                this_config.General.workArea = "crab/{}_{}/".format(requestname_base, production_tag)
+                this_config.General.requestName = "{}_{}_{}_{}".format(requestname_base, production_tag, info["year"], dataset_shortname)
+
+                this_config.section_('JobType')
+                this_config.JobType.pluginName = 'Analysis'
+                this_config.JobType.psetName = os.path.expandvars(info.get("pset", None))
+                this_config.JobType.allowUndistributedCMSSW = True
+                this_config.JobType.numCores = 4
+                this_config.JobType.maxMemoryMB = 8000
+                this_config.JobType.pyCfgParams = [
+                        'isMC={}'.format(isMC), 
+                        'reportEvery=1000',
+                        'tag={}'.format(production_tag),
+                ]
+
+                this_config.section_('User')
+                this_config.section_('Site')
+                this_config.Site.storageSite = output_site
+
+                this_config.section_('Data')
+                this_config.Data.publication = True
+                this_config.Data.outLFNDirBase = "{}/{}/{}".format(output_lfn_base, info["year"], sample)
+                this_config.Data.outputDatasetTag = dataset_shortname
+
+                # Outputs land at outLFNDirBase/outputDatasetTag
+                this_config.Data.inputDBS = 'global'
+                if "private" in info.keys():
+                    if info["private"]:
+                        this_config.Data.inputDBS = 'phys03'
+                this_config.Data.inputDataset = dataset
+                splitting_mode = info.get("splitting", "Automatic")
+                if not splitting_mode in ["Automatic", "FileBased", "LumiBased"]:
+                    raise ValueError("Unrecognized splitting mode: {}".format(splitting_mode))
+                this_config.Data.splitting = splitting_mode
+
+                if not isMC:
+                        this_config.Data.lumiMask = info.get('lumimask', None)
+                else:
+                        this_config.Data.lumiMask = ''
+
+                unitsPerJob = info.get("unitsPerJob", None)
+                if unitsPerJob is not None:
+                    this_config.Data.unitsPerJob = unitsPerJob
+
+                totalUnits = info.get("totalUnits", None)
+                if totalUnits is not None:
+                    this_config.Data.totalUnits = totalUnits
+
+                allowInvalid = info.get("allowInvalid", False)
+                if allowInvalid:
+                    this_config.Data.allowNonValidInputDataset = True
+                
+                print(this_config)
+                p = Process(target=submit, args=(this_config,))
+                p.start()
+                p.join()
+                #submit(this_config)
+            print("*** Done with Sample {} ***\n\n".format(sample))
+
